62d06af9c40cb591b7f26264b831f217cc240f32
Iram <iramsabak@gmail.com>
2025-05-20 11:33:24 +0530

    server

--- src/server.ts
+++ src/server.ts
@@ -1,77 +1,157 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { WebSocketServer } from "ws";
-import connectDB from "./config/db";
-import { verifySocketToken } from "./middlewares/authMiddleware";
-import { Request, Response } from "express";
-import { createServer } from "http";
-import { IncomingMessage } from "http";
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import { WebSocketServer } from "ws";
+// import connectDB from "./config/db";
+// import { verifySocketToken } from "./middlewares/authMiddleware";
+// import { Request, Response } from "express";
+// import { createServer } from "http";
+// import { IncomingMessage } from "http";
 
 
-// Import Routes
-import authRoutes from "./routes/authRoute";
-import userRoutes from "./routes/userRoute";
-import courseRoutes from "./routes/CourseRoute";
-import gameRoutes from "./routes/gameRoute"
-import progressRoutes from "./routes/progressRoute";
-import uploadRoutes from "./routes/uploadRoutes"
-import geminiRoutes from "./routes/chatRoute"
-import { userModel } from "./models/schema";
-import { Messagemodel } from "./models/schema";
+// // Import Routes
+// import authRoutes from "./routes/authRoute";
+// import userRoutes from "./routes/userRoute";
+// import courseRoutes from "./routes/CourseRoute";
+// import gameRoutes from "./routes/gameRoute"
+// import progressRoutes from "./routes/progressRoute";
+// import uploadRoutes from "./routes/uploadRoutes"
+// import geminiRoutes from "./routes/chatRoute"
+// import { userModel } from "./models/schema";
+// import { Messagemodel } from "./models/schema";
 
 
-dotenv.config();
-connectDB();
+// dotenv.config();
+// connectDB();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-const WS_PORT = 8089; // WebSocket Server Port
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+// const WS_PORT = 8089; // WebSocket Server Port
 
-// app.use(express.json());
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+// // app.use(express.json());
+// app.use(express.json({ limit: '100mb' }));
+// app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(cors());
+// app.use(cors());
 
-// API Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/courses", courseRoutes);
-app.use("/api/quiz", gameRoutes);
-app.use("/api/progress", progressRoutes);
-app.use('/api/uploads', uploadRoutes);
-app.use('/api/gemini', geminiRoutes);
-app.get('/dashboard', (req, res) => { res.send(`Welcome `); });
+// // API Routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/courses", courseRoutes);
+// app.use("/api/quiz", gameRoutes);
+// app.use("/api/progress", progressRoutes);
+// app.use('/api/uploads', uploadRoutes);
+// app.use('/api/gemini', geminiRoutes);
+// app.get('/dashboard', (req, res) => { res.send(`Welcome `); });
 
 
-// Start Express Server
- app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT}`);
-});
+// // Start Express Server
+//  app.listen(PORT, () => {
+//   console.log(` Server running on port ${PORT}`);
+// });
 
-//  WebSocket (Chat) 
+// //  WebSocket (Chat) 
+// // const wss = new WebSocketServer({ port: WS_PORT });
+// // Create the HTTP server from your Express app
+// // const server = createServer(app);
+
+// // Now, create the WebSocket server using the same HTTP server
+// // const wss = new WebSocketServer({ port: WS_PORT });
+// // const clients = new Map(); // Store connected users
+
+
+
+// // wss.on("connection", (ws, req: IncomingMessage) => {
+// //   console.log("New client connected");
+// //   try {
+// //     const user = verifySocketToken(req); //  Use your middleware
+// //     //@ts-ignore
+// //     ws.user = user;   // attach to socket
+
+// //     console.log("WebSocket connected:");
+// //     clients.set(ws, {}); // will be updated in join
+// //   } catch (err) {
+// //     // console.error("WebSocket auth failed:", err);
+// //     ws.close(4001, "Unauthorized");
+// //     return;
+// //   }
+
+// //   ws.on("message", async (data) => {
+// //     try {
+// //       const parsed = JSON.parse(data.toString());
+// //       const { type, courseId, message, user } = parsed;
+// //       const userr = await userModel.findById({ _id: user })
+// //       const username = userr?.name
+
+
+// //       if (type === "join") {
+// //         // Save client info
+// //         clients.set(ws, { courseId, username });
+
+// //         // Fetch previous chat from DB
+// //         const chatHistory = await Messagemodel.find({ courseId }).sort({ createdAt: 1 });
+
+// //         // Send chat history to this user only
+// //         ws.send(JSON.stringify({ type: "history", data: chatHistory, username }));
+
+// //         console.log(`${user} joined course ${courseId}`);
+// //       }
+
+// //       if (type === "message") {
+// //         // Save message to DB
+// //         await Messagemodel.create({ username, user, courseId, message });
+
+
+// //         // Broadcast to others
+
+
+// //         wss.clients.forEach((client) => {
+// //           if (
+// //             client.readyState === ws.OPEN &&
+// //             clients.get(client)?.courseId === courseId
+// //           ) {
+// //             client.send(JSON.stringify({ type: "message", username, message }));
+// //           }
+// //         })
+// //       }
+
+// //     } catch (error) {
+// //       console.error("Invalid message format", error);
+// //     }
+// //   });
+
+// //   ws.on("close", () => {
+// //     clients.delete(ws);
+// //     console.log("Client disconnected");
+// //   });
+// // });
+
+
+// // console.log(` WebSocket server running on port ${WS_PORT}`);
+// // import { WebSocketServer } from "ws";
+// // import { IncomingMessage } from "http";
+// // import jwt from "jsonwebtoken";
+// // import { userModel, Messagemodel } from "./models/schema";
+// // import { verifySocketToken } from "./utils/token"; // Your token decoder
+// import { WebSocket } from "ws";
+
+// interface ClientInfo {
+//   courseId: string;
+//   username: string;
+// }
+
+// const clients = new Map<WebSocket, ClientInfo>();
 // const wss = new WebSocketServer({ port: WS_PORT });
-// Create the HTTP server from your Express app
-// const server = createServer(app);
 
-// Now, create the WebSocket server using the same HTTP server
-// const wss = new WebSocketServer({ port: WS_PORT });
-// const clients = new Map(); // Store connected users
-
-
-
-// wss.on("connection", (ws, req: IncomingMessage) => {
+// wss.on("connection", async (ws, req: IncomingMessage) => {
 //   console.log("New client connected");
-//   try {
-//     const user = verifySocketToken(req); //  Use your middleware
-//     //@ts-ignore
-//     ws.user = user;   // attach to socket
 
-//     console.log("WebSocket connected:");
-//     clients.set(ws, {}); // will be updated in join
+//   try {
+//     const decodedUser = verifySocketToken(req);
+//     // @ts-ignore
+//     ws.user = decodedUser;
+//     clients.set(ws, {} as ClientInfo); // initially empty
 //   } catch (err) {
-//     // console.error("WebSocket auth failed:", err);
 //     ws.close(4001, "Unauthorized");
 //     return;
 //   }
@@ -79,44 +159,39 @@ app.get('/dashboard', (req, res) => { res.send(`Welcome `); });
 //   ws.on("message", async (data) => {
 //     try {
 //       const parsed = JSON.parse(data.toString());
-//       const { type, courseId, message, user } = parsed;
-//       const userr = await userModel.findById({ _id: user })
-//       const username = userr?.name
+//       const { type, courseId, message } = parsed;
 
+//       // @ts-ignore
+//       const userId = ws.user?.id;
+//       const userr = await userModel.findById(userId);
+//       if (!userr || !userr.name) return;
+//       const username = userr.name;
 
 //       if (type === "join") {
-//         // Save client info
 //         clients.set(ws, { courseId, username });
 
-//         // Fetch previous chat from DB
 //         const chatHistory = await Messagemodel.find({ courseId }).sort({ createdAt: 1 });
 
-//         // Send chat history to this user only
 //         ws.send(JSON.stringify({ type: "history", data: chatHistory, username }));
-
-//         console.log(`${user} joined course ${courseId}`);
+//         console.log(`${username} joined course ${courseId}`);
 //       }
 
 //       if (type === "message") {
-//         // Save message to DB
-//         await Messagemodel.create({ username, user, courseId, message });
-
-
-//         // Broadcast to others
-
+//         await Messagemodel.create({ username, user: userId, courseId, message });
 
 //         wss.clients.forEach((client) => {
+//           const info = clients.get(client as WebSocket);
 //           if (
-//             client.readyState === ws.OPEN &&
-//             clients.get(client)?.courseId === courseId
+//             client.readyState === WebSocket.OPEN &&
+//             info?.courseId === courseId
 //           ) {
 //             client.send(JSON.stringify({ type: "message", username, message }));
 //           }
-//         })
+//         });
 //       }
 
-//     } catch (error) {
-//       console.error("Invalid message format", error);
+//     } catch (err) {
+//       console.error("Invalid message format", err);
 //     }
 //   });
 
@@ -126,14 +201,55 @@ app.get('/dashboard', (req, res) => { res.send(`Welcome `); });
 //   });
 // });
 
+// console.log(`✅ WebSocket server running on port ${WS_PORT}`);
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { WebSocketServer } from "ws";
+import connectDB from "./config/db";
+import { verifySocketToken } from "./middlewares/authMiddleware";
+import { IncomingMessage } from "http";
+import { createServer } from "http";
 
-// console.log(` WebSocket server running on port ${WS_PORT}`);
-// import { WebSocketServer } from "ws";
-// import { IncomingMessage } from "http";
-// import jwt from "jsonwebtoken";
-// import { userModel, Messagemodel } from "./models/schema";
-// import { verifySocketToken } from "./utils/token"; // Your token decoder
+import authRoutes from "./routes/authRoute";
+import userRoutes from "./routes/userRoute";
+import courseRoutes from "./routes/CourseRoute";
+import gameRoutes from "./routes/gameRoute";
+import progressRoutes from "./routes/progressRoute";
+import uploadRoutes from "./routes/uploadRoutes";
+import geminiRoutes from "./routes/chatRoute";
+
+import { userModel, Messagemodel } from "./models/schema";
 import { WebSocket } from "ws";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+
+// API Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/courses", courseRoutes);
+app.use("/api/quiz", gameRoutes);
+app.use("/api/progress", progressRoutes);
+app.use("/api/uploads", uploadRoutes);
+app.use("/api/gemini", geminiRoutes);
+
+app.get("/dashboard", (req, res) => {
+  res.send(`Welcome`);
+});
+
+// Create HTTP server wrapping Express app
+const server = createServer(app);
+
+// Create WebSocket server attached to the same HTTP server
+const wss = new WebSocketServer({ server });
 
 interface ClientInfo {
   courseId: string;
@@ -141,7 +257,6 @@ interface ClientInfo {
 }
 
 const clients = new Map<WebSocket, ClientInfo>();
-const wss = new WebSocketServer({ port: WS_PORT });
 
 wss.on("connection", async (ws, req: IncomingMessage) => {
   console.log("New client connected");
@@ -150,7 +265,7 @@ wss.on("connection", async (ws, req: IncomingMessage) => {
     const decodedUser = verifySocketToken(req);
     // @ts-ignore
     ws.user = decodedUser;
-    clients.set(ws, {} as ClientInfo); // initially empty
+    clients.set(ws, {} as ClientInfo);
   } catch (err) {
     ws.close(4001, "Unauthorized");
     return;
@@ -170,9 +285,13 @@ wss.on("connection", async (ws, req: IncomingMessage) => {
       if (type === "join") {
         clients.set(ws, { courseId, username });
 
-        const chatHistory = await Messagemodel.find({ courseId }).sort({ createdAt: 1 });
+        const chatHistory = await Messagemodel.find({ courseId }).sort({
+          createdAt: 1,
+        });
 
-        ws.send(JSON.stringify({ type: "history", data: chatHistory, username }));
+        ws.send(
+          JSON.stringify({ type: "history", data: chatHistory, username })
+        );
         console.log(`${username} joined course ${courseId}`);
       }
 
@@ -185,11 +304,12 @@ wss.on("connection", async (ws, req: IncomingMessage) => {
             client.readyState === WebSocket.OPEN &&
             info?.courseId === courseId
           ) {
-            client.send(JSON.stringify({ type: "message", username, message }));
+            client.send(
+              JSON.stringify({ type: "message", username, message })
+            );
           }
         });
       }
-
     } catch (err) {
       console.error("Invalid message format", err);
     }
@@ -201,4 +321,6 @@ wss.on("connection", async (ws, req: IncomingMessage) => {
   });
 });
 
-console.log(`✅ WebSocket server running on port ${WS_PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
